723d2a938419668a689aace5e4e6ac276da77983
MiniApollo <surmannmark@gmail.com>
2023-07-26 16:19:50 +0200

    changed characterbody to physicsbody, fixed deselectall not hiding marker

--- RTS_UnitSelection/src/scripts/Unit_Selection.cs
+++ RTS_UnitSelection/src/scripts/Unit_Selection.cs
@@ -1,45 +1,41 @@
 using Godot;
-using System;
 using System.Collections.Generic;
 
-namespace Unit_Selection{
+// TODO UnitMovement
+static class Unit_Selection {
 
-	// TODO UnitMovement
-	static class Unit_Selection {
+	public static void ClickSelect(PhysicsBody3D UnitToAdd, List<PhysicsBody3D> UnitsSelected) {
 
-		public static void ClickSelect(CharacterBody3D UnitToAdd, List<CharacterBody3D> UnitsSelected) {
-
-			DeselectAll(UnitsSelected);
+		DeselectAll(UnitsSelected);
+		UnitsSelected.Add(UnitToAdd);
+		UnitToAdd.GetChild<MeshInstance3D>(0).Show();
+	}
+	public static void ShiftClickSelect(PhysicsBody3D UnitToAdd, List<PhysicsBody3D> UnitsSelected) {
+		if (!UnitsSelected.Contains(UnitToAdd)) {
 			UnitsSelected.Add(UnitToAdd);
 			UnitToAdd.GetChild<MeshInstance3D>(0).Show();
 		}
-		public static void ShiftClickSelect(CharacterBody3D UnitToAdd, List<CharacterBody3D> UnitsSelected) {
-			if (!UnitsSelected.Contains(UnitToAdd)) {
-				UnitsSelected.Add(UnitToAdd);
-				UnitToAdd.GetChild<MeshInstance3D>(0).Show();
-			}
-			else {
-				UnitToAdd.GetChild<MeshInstance3D>(0).Hide();
-				UnitsSelected.Remove(UnitToAdd);
-			}
+		else {
+			UnitToAdd.GetChild<MeshInstance3D>(0).Hide();
+			UnitsSelected.Remove(UnitToAdd);
 		}
-		public static void DragSelect(CharacterBody3D UnitToAdd, List<CharacterBody3D> UnitsSelected) {
-			if (!UnitsSelected.Contains(UnitToAdd)) {
+	}
+	public static void DragSelect(PhysicsBody3D UnitToAdd, List<PhysicsBody3D> UnitsSelected) {
+		if (!UnitsSelected.Contains(UnitToAdd)) {
 
-				UnitsSelected.Add(UnitToAdd);
-				UnitToAdd.GetChild<MeshInstance3D>(0).Show();
-			}
+			UnitsSelected.Add(UnitToAdd);
+			UnitToAdd.GetChild<MeshInstance3D>(0).Show();
+		}
 
-		}
-		public static void Deselect(CharacterBody3D UnitToDeselect, List<CharacterBody3D> UnitsSelected) {
-			UnitsSelected.Remove(UnitToDeselect);
-		}
-		public static void DeselectAll(List<CharacterBody3D> UnitsSelected) {
-			for (int i = 0; i < UnitsSelected.Count; i++) {
+	}
+	public static void Deselect(PhysicsBody3D UnitToDeselect, List<PhysicsBody3D> UnitsSelected) {
+		UnitsSelected.Remove(UnitToDeselect);
+	}
+	public static void DeselectAll(List<PhysicsBody3D> UnitsSelected) {
+		for (int i = 0; i < UnitsSelected.Count; i++) {
 
-				UnitsSelected[i].GetChild<MeshInstance3D>(0).Show();
-			}
-			UnitsSelected.Clear();
+			UnitsSelected[i].GetChild<MeshInstance3D>(0).Hide();
 		}
+		UnitsSelected.Clear();
 	}
 }
